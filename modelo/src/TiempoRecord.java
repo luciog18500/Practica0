c6b633f8dac99c408c99684beedb387d28f23c33
luciog18500 <luciog18500@gmail.com>
2024-03-12 23:18:47 -0300

    comentado record

---
+++ modelo/src/TiempoRecord.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public record TiempoRecord() {
+
+//    public static Date fecha;
+//
+//    TiempoRecord(Date fecha) {
+//        this.fecha = fecha;
+//    }
+//
+//
+//    public Date getFecha() {
+//        return fecha;
+//    }
+//
+//    public void setFecha(Date fecha) {
+//        this.fecha = fecha;
+//    }
+
+
+}
